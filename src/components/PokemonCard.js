f6e0c6b36f4d72aa9b2f0b7ecb4637b5b6d8ba38
DESKTOP-VF4GDHS\Martyna <martiaaarti@gmail.com>
2019-03-03 18:09:50 +0100

    Color change to fix

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,11 +2,47 @@ import React, { Component} from 'react';
 import { connect } from "react-redux";
 import { getData } from "../actions/index";
 import { CardTitle } from 'reactstrap';
+import '../styles/colors.scss';
 
 export class PokemonCard extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.getForceColor = this.getForceColor.bind(this);
+    }
 
     componentDidMount() {
         this.props.getData();
+    }
+
+    getForceColor(forceType) {
+        const colorOfForces = {
+            Grass: {
+                background: '$font-color',
+                color: '$grass-color'
+            },
+            Poison: {
+                background: '$font-color',
+                color: '$poison-color'
+            },
+            Fire: {
+                background: '$font-color',
+                color: '$fire-color'
+            },
+            Flying: {
+                background: '$font-color',
+                color: '$flying-color'
+            },
+            Water: {
+                background: '$font-color',
+                color: '$water-color'
+            },
+            Bug: {
+                background: '$font-color',
+                color: '$bug-color'
+            }
+        };
+        return colorOfForces[forceType];
     }
 
     render() {
@@ -18,7 +54,8 @@ export class PokemonCard extends Component {
                                     <CardTitle className='p-2'># {pokemon.num} {pokemon.name}</CardTitle>
                                     <div className='align-bottom'>
                                         {pokemon.type.map((force, index) => (
-                                            <span size='sm' className='badge p-1 mx-1' key={index} >
+                                            <span size='sm' className='badge p-1 mx-1 grass-color' key={index}  >
+                                                {/* {this.getForceColor(force)} */}
                                                 {force}
                                             </span>
                                         ))}
