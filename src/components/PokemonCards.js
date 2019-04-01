cf706338c36588ae8ca87d3b2b10613544122244
Martyna <martiaaarti@gmail.com>
2019-04-01 14:36:43 +0200

    Pagination button

--- src/components/PokemonCards.js
+++ src/components/PokemonCards.js
@@ -30,22 +30,30 @@ export class PokemonCards extends Component {
         }
 
         return (
-            <div className='parent m-1'>
-                {pokemons.map((pokemon) => (
-                    <div key={pokemon.id} className='card text-center'>
-                        <Link to={'/details/' + pokemon.id} className='link-decoration' style={{ textDecoration: 'none' }}>
-                            <img className='card-img-top p-4' height='80%' src={pokemon.img} alt='Pokemon thumbnail' />
-                            <h4 className='card-title p-2'>#{pokemon.num} {pokemon.name}</h4>
-                        </Link>
-                        <div className='align-bottom mb-2'>
-                            {pokemon.type.map((force, i) => (
-                                <span size='sm' className={'badge p-1 mx-1' + force} key={i} >
-                                    {force}
-                                </span>
-                            ))}
+            <div>
+                <nav aria-label="navigation">
+                    <ul className="pagination justify-content-center pagination-decoration">
+                        <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                        <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                    </ul>
+                </nav>
+                <div className='parent m-1'>
+                    {pokemons.map((pokemon) => (
+                        <div key={pokemon.id} className='card text-center'>
+                            <Link to={'/details/' + pokemon.id} className='link-decoration' style={{ textDecoration: 'none' }}>
+                                <img className='card-img-top p-4' height='80%' src={pokemon.img} alt='Pokemon thumbnail' />
+                                <h4 className='card-title p-2'>#{pokemon.num} {pokemon.name}</h4>
+                            </Link>
+                            <div className='align-bottom mb-2'>
+                                {pokemon.type.map((force, i) => (
+                                    <span size='sm' className={'badge p-1 mx-1' + force} key={i} >
+                                        {force}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         )
     }
