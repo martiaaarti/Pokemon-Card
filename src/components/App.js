01ed106e2e55582927d5bfb9800d8fc6ae97ca18
Martyna <martiaaarti@gmail.com>
2019-04-24 09:12:55 +0200

    Document formated

--- src/components/App.js
+++ src/components/App.js
@@ -4,31 +4,24 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTimes, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import PokemonCards from './PokemonCards';
 import PokemonDetails from './PokemonDetails';
-//import { getData } from '../actions/index';
 import { connect } from 'react-redux';
 library.add(faTimes, faSpinner);
 
 export class App extends Component {
 
-    // componentDidMount() {
-    //     this.props.getData();
-    // }
-
     render() {
         return (
             <Router>
-                    <Switch>
-                        <Route exact path='/' render={() => <Redirect to='/1'/>}/>
-                        <Route path='/details/:id' component={PokemonDetails} />
-                        <Route exact path='/:currentPageNumber' component={PokemonCards}/>
-                    </Switch>
+                <Switch>
+                    <Route exact path='/' render={() => <Redirect to='/1' />} />
+                    <Route path='/details/:id' component={PokemonDetails} />
+                    <Route exact path='/:currentPageNumber' component={PokemonCards} />
+                </Switch>
             </Router>
         )
     }
 };
 
 export default connect(
-    undefined
-    // ,
-    // { getData }
+    undefined,
 )(App);
